fc0eb56aa8c71c6f5d0a2aba1a29cfae2859dac6
biancads11 <biancadscosta@gmail.com>
2025-08-29 22:38:57 -0400

    Remove custom login action, enforce token-based authentication, and add pagination configuration.

--- core/views.py
+++ core/views.py
@@ -1,19 +1,15 @@
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import Group, Permission
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
 
 from core import serializers, models, filters
 from core.mixins import HistoryViewSetMixin
 
 
 class BaseViewSet(viewsets.ModelViewSet, HistoryViewSetMixin):
-    # permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     ordering = ['created_at']
 
@@ -26,21 +22,6 @@ class UserViewSet(BaseViewSet):
     serializer_class = serializers.UserSerializer
     filterset_class = filters.UserFilter
     ordering_fields = ['__all__']
-
-    # apagar action de login e usar endpoint de token para pegar os tokens
-    @action(detail=False, methods=['POST'], permission_classes = [AllowAny])
-
-    def login(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            refresh = RefreshToken.for_user(user)
-
-            return Response({"token": str(refresh.access_token)}, status=status.HTTP_200_OK)
-        return Response({"detail": "Usuário não encontrado"}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=True, methods=['get'], url_path='history')
     def get_history(self, request, pk=None):
